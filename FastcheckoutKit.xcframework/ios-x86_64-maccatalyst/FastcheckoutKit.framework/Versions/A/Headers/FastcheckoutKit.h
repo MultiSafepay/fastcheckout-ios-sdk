c4b60e61f49d4ad0024d2ac77efad1bd97cbb339
Javier Algaba <javier.algaba@multisafepay.com>
2022-03-22 17:41:50 +0100

    Lock XCFramework to Xcode version 12.0

---
+++ FastcheckoutKit.xcframework/ios-x86_64-maccatalyst/FastcheckoutKit.framework/Versions/A/Headers/FastcheckoutKit.h
@@ -0,0 +1,17 @@
+//
+//  FastcheckoutKit.h
+//  FastcheckoutKit
+//
+//  Copyright © 2016 MultiSafepay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FastcheckoutKit.
+FOUNDATION_EXPORT double FastcheckoutKitVersionNumber;
+
+//! Project version string for FastcheckoutKit.
+FOUNDATION_EXPORT const unsigned char FastcheckoutKitVersionString[];
+
+#define FastcheckoutLiveEnvironment @"Live"
+#define FastcheckoutTestEnvironment @"Test"
